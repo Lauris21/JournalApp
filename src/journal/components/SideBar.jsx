4da24af21791858f090cdad461872eac12343ebb
Laura <laurasanchez0301@gmail.com>
2023-11-24 23:09:52 +0100

    sidebar

---
+++ src/journal/components/SideBar.jsx
@@ -0,0 +1,59 @@
+import PropType from "prop-types";
+import {
+  Box,
+  Divider,
+  Drawer,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { TurnedInNot } from "@mui/icons-material";
+export const SideBar = ({ drawerWidth }) => {
+  return (
+    <Box
+      component="nav"
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    >
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: "block" },
+          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Lil
+          </Typography>
+        </Toolbar>
+        <Divider />
+        <List>
+          {["Enero", "Febrero", "Marzo", "Abril"].map((item) => (
+            <ListItem Key={item} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <TurnedInNot />
+                </ListItemIcon>
+                <Grid container>
+                  <ListItemText primary={item} />
+                  <ListItemText
+                    secondary={" openVoluptate duis ut amet mollit ullamco."}
+                  />
+                </Grid>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
+
+SideBar.propTypes = {
+  drawerWidth: PropType.number,
+};
--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,14 +1,16 @@
 import PropTypes from "prop-types";
-import { Box } from "@mui/material";
-import { NavBar } from "../components";
+import { Box, Toolbar } from "@mui/material";
+import { NavBar, SideBar } from "../components";
 
-const drawerWidth = 240;
+const drawerWidth = 280;
 
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
       <NavBar drawerWidth={drawerWidth} />
+      <SideBar drawerWidth={drawerWidth} />
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
         {children}
       </Box>
     </Box>
